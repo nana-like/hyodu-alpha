49a204b0cc35acefe2fb5cd3ba855c3298b25755
nana <nykim@nykim.net>
2020-02-27 15:43:22 +0900

    distribute

--- ui.js
+++ ui.js
@@ -1,43 +1,53 @@
-var animation = function () {
-  var items, winH;
+(function () {
 
-  var initModule = function () {
-    items = document.querySelectorAll(".fadeIn");
-    winH = window.innerHeight;
-    _addEventHandlers();
-  }
+  var animation = function () {
+    var items, winH;
 
-  var _addEventHandlers = function () {
-    window.addEventListener("scroll", _checkPosition);
-    window.addEventListener("load", _checkPosition);
-    window.addEventListener("resize", initModule);
-  };
+    var initModule = function () {
+      items = document.querySelectorAll(".fadeIn");
+      winH = window.innerHeight;
+      _addEventHandlers();
+    }
 
-  var _checkPosition = function () {
-    for (var i = 0; i < items.length; i++) {
-      var posFromTop = items[i].getBoundingClientRect().top;
-      if (winH > posFromTop + 100) {
-        items[i].classList.add("active");
+    var _addEventHandlers = function () {
+      window.addEventListener("scroll", _checkPosition);
+      window.addEventListener("load", _checkPosition);
+      window.addEventListener("resize", initModule);
+    };
+
+    var _checkPosition = function () {
+      for (var i = 0; i < items.length; i++) {
+        var posFromTop = items[i].getBoundingClientRect().top;
+        if (winH > posFromTop + 100) {
+          items[i].classList.add("active");
+        }
       }
+    }
+
+    return {
+      init: initModule
     }
   }
 
-  return {
-    init: initModule
+
+  var heroSection = document.querySelector(".hero");
+  var girlEyes = document.querySelector(".deco-girl-eyes-container");
+  var winW = window.innerWidth;
+
+  var getWinW = function () {
+    winW = window.innerWidth;
   }
-}
+  var eyeMotion = function (e) {
+    var posX = ((e.clientX / winW * 2) - 1) * 2.1;
+    var posY = ((e.clientY / winW * 2) - 1) * 2.5;
+    if (winW < 1060) {
+      var posX = ((e.clientX / winW * 2) - 1) * 1.2;
+      var posY = ((e.clientY / winW * 2) - 1) * 1.5;
+    }
+    girlEyes.style.transform = `translate(${posX}px, ${posY}px)`;
+  }
 
-animation().init();
-
-
-
-
-
-var heroSection = document.querySelector(".hero");
-var girlEyes = document.querySelector(".deco-girl-eyes-container");
-var eyeMotion = function (e) {
-  var posX = ((e.clientX / window.innerWidth * 2) - 1) * 2.1;
-  var posY = ((e.clientY / window.innerWidth * 2) - 1) * 2.5;
-  girlEyes.style.transform = `translate(${posX}px, ${posY}px)`;
-}
-heroSection.addEventListener("mousemove", eyeMotion);
+  animation().init();
+  window.addEventListener("resize", getWinW);
+  heroSection.addEventListener("mousemove", eyeMotion);
+})();
